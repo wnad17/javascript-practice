66fe8cd7c125360ec35a3ac65f1951ae8f3e6e9f
Christine Dawn Capatan <dawncapatan17@gmail.com>
2026-07-21 05:40:09 +0800

    Add string method using trim(), toUpperCase(), and toLowerCase() methods

--- string-methods.js
+++ string-methods.js
@@ -29,3 +29,37 @@ const endPunctuation = message.slice(-1);
 console.log(`The ending punctuation mark is a "${endPunctuation}"`);
 
 console.log("Workshop complete! You now know how to use includes() and slice().");
+
+//==============================================
+// Lesson 2. Learn how to use trim(), toUpperCase(), and toLowerCase() methods
+//==============================================
+
+const userInput = "   Hello World!   ";
+console.log("Original input:");
+console.log(userInput);
+
+const cleanedInput = userInput.trim();
+console.log("Result of trimming whitespace from both ends:");
+console.log(cleanedInput);
+
+const trimmedStart = userInput.trimStart();
+console.log("After using the trimStart() method, leading spaces removed:");
+console.log(trimmedStart);
+
+const trimmedEnd = userInput.trimEnd();
+console.log("After using the trimEnd() method, trailing spaces removed:");
+console.log(trimmedEnd);
+
+const upperCaseInput = cleanedInput.toUpperCase();
+console.log("Result of using the toUpperCase() method:");
+console.log(upperCaseInput);
+
+const lowerCaseInput = cleanedInput.toLowerCase();
+console.log("Result of using the toLowerCase() method:");
+console.log(lowerCaseInput);
+
+const lowercaseWord = "camelcase";
+const camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase() + lowercaseWord.slice(-3)
+console.log("Camel cased version:");
+console.log(camelCasedVersion);
+
